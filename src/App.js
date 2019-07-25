6df75c22c8b8a16942a915b500673e90fba61e1c
Lourie Basson <lourie.basson@hetzner.co.za>
2019-07-25 15:13:45 +0200

    Yolande initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,62 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  function calculateDate() {
+    let month = new Date().getMonth();
+    let year = new Date().getFullYear();
+
+    let paydays = [];
+
+    for (let i = month; i < 12; i++) {
+      let payDayOfMonth = new Date(year, i, 25);
+
+      let day = payDayOfMonth.getDay();
+
+      if (day === 0) {
+        payDayOfMonth = new Date(year, i, 23);
+        paydays.push(payDayOfMonth.toString());
+      } else if (day === 6) {
+        payDayOfMonth = new Date(year, i, 24);
+        paydays.push(payDayOfMonth.toString());
+      } else {
+        paydays.push(payDayOfMonth.toString());
+      }
+    }
+
+    return paydays;
+  }
+
+  let paydays = calculateDate();
+  let salary = 37500;
+
+  function calculateBonusDay() {
+    let year = new Date().getFullYear();
+    let bonusDay = new Date(year, 11, 25);
+
+    let day = bonusDay.getDay();
+
+    if (day == 3) {
+      bonusDay = new Date(year, 11, 23).toString();
+      return bonusDay;
+    }
+  }
+
+  let bonus = (10 / 100) * salary;
+
   return (
-    <div className='app'>
-      <div className='app-inner'></div>
+    <div className="app">
+      <div className="app-inner">
+        <h2>Pay day Calendar</h2>
+        <p>R {salary}.00</p>
+        {paydays.map(day => (
+          <div key={day}>{day}</div>
+        ))}
+
+        <h2>Bonus pay day</h2>
+        <div>R {bonus}.00</div>
+        <div>{calculateBonusDay()}</div>
+      </div>
     </div>
   );
 }
